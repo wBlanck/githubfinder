5198336de640cfada193a20cb5cda9851c8339dc
wblanck <blanck.william@gmail.com>
2022-02-15 11:59:48 +0100

    token

--- src/components/users/UserResult.jsx
+++ src/components/users/UserResult.jsx
@@ -5,19 +5,28 @@ import UserItem from "./UserItem";
 function UserResult() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     fetchUsers();
   }, []);
-  const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
-    const data = await response.json();
 
-    setUsers(data);
-    setLoading(false);
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_GITHUB_URL}/users`,
+        {
+          headers: {
+            Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+          },
+        }
+      );
+      const data = await response.json();
+
+      setUsers(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   if (!loading) {
